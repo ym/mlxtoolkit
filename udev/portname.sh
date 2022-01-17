431a455e1c25bc6503baf5aed634c51578ca7996
Alexander Zubkov <green@qrator.net>
2022-01-17 17:16:14 +0100

    add means to rename ports with udev

---
+++ udev/portname.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+if [ "$1" = 'install' ]; then
+	cat >/etc/udev/rules.d/70-net-mlxtoolkit.rules <<EOF
+SUBSYSTEM=="net", ACTION=="add", NAME=="", ATTR{phys_port_name}!="", PROGRAM=="$(realpath "$0")", NAME="\$result"
+EOF
+	exit $?
+fi
+exec 2>/dev/null # ignore missing phys_port_name
+sed 's/^p\([0-9]+\)/port\1/;s/s\([0-9]+\)$/-\1/' </sys/"$DEVPATH"/phys_port_name
